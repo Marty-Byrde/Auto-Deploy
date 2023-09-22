7594d3d340f8aece5bf8707ef0f6e73fb785ef10
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-22 17:22:33 +0200

    feat: added deployment-item interface

--- index.ts
+++ index.ts
@@ -1,5 +1,29 @@
+import express, { Express, Request, Response } from "express"
+import bodyParser from "body-parser"
+import queue from 'express-queue';
+import * as dotenv from 'dotenv'
+import { AdvancedCollection, DBHandler } from 'mongodb_handler'
+import { NodeSSH, SSHExecCommandResponse } from 'node-ssh'
 
+dotenv.config({ path: './.env' })
+const app: Express = express()
+const sshClient = new NodeSSH()
 
-export async function myFunction(){
+let dbHandler: DBHandler
+let collection: AdvancedCollection;
 
+interface AutoDeployItem {
+  key: string
+  vps_ip: string
+  vps_Credentials: {
+    username: string
+    password: string
+  },
+  scriptLines: ScriptLine[],
+}
+
+interface ScriptLine {
+  command: string
+  args?: string[],
+  passwordRequired?: boolean
 }

95f93240103e9cfc9e4f1193a47a515d91c298c9
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-22 17:23:13 +0200

    feat: added initialization function
    This functions establishes a connection to the database and sets up the rate limits for the auto-deploy page.

--- index.ts
+++ index.ts
@@ -27,3 +27,26 @@ interface ScriptLine {
   args?: string[],
   passwordRequired?: boolean
 }
+
+
+
+init()
+
+async function init() {
+  console.log("Initializing database connection...")
+  dbHandler = new DBHandler(process.env.MONGO_HOST, parseInt(process.env.MONGO_PORT))
+  await dbHandler.connect()
+
+  collection = await dbHandler.getCollection(process.env.MONGO_DB, process.env.MONGO_COLLECTION)
+  const items = await collection.findType<AutoDeployItem>({})
+  console.log(`${items.length} auto-deploy entries found in the database.`)
+
+  console.log(`Configuring the rate limits for this express-app...`)
+  app.listen(process.env.PORT, async () => {
+    console.log(`Server has been started on port ${process.env.PORT}`)
+  })
+
+  const rateLimt = Math.round(items.length * 1.5)
+  app.use(queue({ activeLimit: rateLimt, queuedLimit: -1 }));
+  console.log(`Active requests limited to ${rateLimt}.`)
+}

a12e18d50e64918e02e203746bf53e5271102c42
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-22 17:29:03 +0200

    feat: simplified job retrieval in deployment-route

--- index.ts
+++ index.ts
@@ -77,12 +77,12 @@ async function executeDeploymentJob(deployment_config: AutoDeployItem) {
 
 app.get(`/${process.env.DEPLOY_ROUTE}/`, async (req: Request, res: Response) => {
   const { key } = req.query
-  const job = await collection.findType<AutoDeployItem>({ key: key })
+  const job = (await collection.findType<AutoDeployItem>({ key: key }))?.at(0)
 
-  if(!job || job.length === 0) return res.sendStatus(200)
+  if(!job) return res.sendStatus(200)
 
   console.log(`An deployment job was found and will be executed...`)
-  await executeDeploymentJob(job[0])
+  await executeDeploymentJob(job)
   console.log(`The deployment-job ${key} has been completed.`)
 
   res.sendStatus(202)
